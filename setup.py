74536fdc495d1d64407da68fb9b4530f22a46114
Maxime Laudrin <maximedelaudrin@gmail.com>
2015-09-23 12:00:01 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='qrec',
+      version='0.0.1',
+      description='Quickly record list of short sound files',
+      url='http://www.github.com/gropax/qrec.py',
+      author='gropax',
+      author_email='maximedelaudrin@gmail.com',
+      license='MIT',
+      packages=['qrec'],
+      scripts=['bin/qrec'],
+      zip_safe=False)
